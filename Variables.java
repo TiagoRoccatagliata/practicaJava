884c15165c51bcb0f9f04b501d66d2014552be41
Tiagoroccatagliata <tiagoroccatagliata@gmail.com>
2024-05-30 10:15:33 -0300

    Implementacion de inputs con scanners

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Main  {
+    public static void  main(String[] args) {
+        String x = "Agua";
+        String y = "Jugo";
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
+
+
